4db0d43bfc54be1edfa1a61c5d291cd72c31a061
kristofcsaszar <csaszar.kristof.felhasznalo@gmail.com>
2022-12-01 17:20:07 +0100

    Day 1: calories

--- 01_calories/01_calories.py
+++ 01_calories/01_calories.py
@@ -1,21 +1,44 @@
-    
+class FixedTopQueue:
+    """
+    This class accepts inputs, and only keeps the top n
+
+    @param: size: specifies the number of max items
+
+    """
+    def __init__(self, size:int) -> None:
+        self.size = size
+        self.list = []
+
+    def push(self, number):
+        if len(self.list) < self.size:
+            self.list.append(number)
+        else:
+            min_list = min(self.list)
+            if number > min_list:
+                self.list.remove(min_list)
+                self.list.append(number)
+
+    def total(self):
+        return sum(self.list)
+
+
 if __name__ == "__main__":
     input_file = open("input.txt", "r")
 
-    max_total = 0
+
+    top_three = FixedTopQueue(3)
     total = 0
 
 
     for line in input_file:
         # jump to new elf
         if line == "\n":
-            if total>=max_total:
-                max_total = total
+            top_three.push(total)
             total = 0
         else:
             total+= int(line)
 
-    print(f"The elf carrying the most has {max_total} calories.")
+    print(f"The top 3 elf carrying the most has {top_three.total()} calories.")
            
 
         
